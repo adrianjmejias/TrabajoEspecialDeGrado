81db4f76a53f7bdf5966c874ae5246bcc95f9de1
adrianjmejias <adrianjmejias@gmail.com>
2021-11-03 15:33:54 +0000

    feat(web): GetRevisions implemented

---
+++ web-app/api/article/useGetRevisions.tsx
@@ -0,0 +1,33 @@
+import { queryFactory } from "../wikimetrix";
+
+export type GetRevisionsResponse = {
+  comment: string;
+  size: number;
+  contentformat: string;
+  pageid: number;
+  tags: [];
+  locale: string;
+  timestamp: string;
+  "*": string;
+  userid: number;
+  revid: number;
+  contentmodel: string;
+  extraction_date: string;
+  parentid: number;
+  title: string;
+  _id: {
+    $oid: "6158b199d61e8295cfe77b12";
+  };
+  minor: "";
+  user: string;
+}[];
+
+const p = queryFactory<GetRevisionsResponse>({
+  url: "revisions",
+  method: "GET",
+});
+
+export const getRevisionsQuery = p.axiosQuery;
+export const useGetRevisionsQuery = p.useAxiosQuery;
+
+export default useGetRevisionsQuery;
