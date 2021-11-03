815b092ba287832574713afe7ea169f8695f262a
adrianjmejias <adrianjmejias@gmail.com>
2021-11-03 15:17:53 +0000

    feat(web): defining types of useGetArticleList

--- web-app/api/article/useGetArticleList.tsx
+++ web-app/api/article/useGetArticleList.tsx
@@ -1,8 +1,18 @@
 import { queryFactory } from "../wikimetrix";
 
-export interface GetArticleListResponse {
-  hola: string;
-}
+export type GetArticleListResponse = {
+  ns: number;
+  pageid: number;
+  title: string;
+  locale: string;
+
+  // TODO: Check these are transformed into dates
+  first_extraction_date: Date;
+  last_extraction_date: Date;
+
+  // Wtf does this mean
+  last_revision_extracted: number;
+}[];
 
 const p = queryFactory<GetArticleListResponse>({
   url: "articles",
--- web-app/pages/article/[articleName].tsx
+++ web-app/pages/article/[articleName].tsx
@@ -2,10 +2,17 @@ import { NextPage } from "next";
 import React from "react";
 import useGetArticleList from "api/article/useGetArticleList";
 
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 const useArticleDetailsPageViewModel = () => {
   const articleQuery = useGetArticleList({
     params: {
-      title: "Orgia",
+      title: undefined && "Orgia",
     },
   });
 
@@ -15,7 +22,43 @@ const useArticleDetailsPageViewModel = () => {
 const ArticleDetailsPage: NextPage = () => {
   const { articleQuery } = useArticleDetailsPageViewModel();
 
-  return <>{JSON.stringify(articleQuery)}</>;
+  return (
+    <div>
+      <pre>{JSON.stringify(articleQuery, null, 2)}</pre>
+
+      <Box display="flex">
+        {articleQuery.data?.map((article) => {
+          return (
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {article.title}
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          );
+        })}
+      </Box>
+    </div>
+  );
 };
 
 export default ArticleDetailsPage;
